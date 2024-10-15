159fc52a8f2184b8ab45ffd7ea3a7c2fcce1672e
Alice Herrera <a.aliceherrera@gmail.com>
2024-10-15 16:10:19 -0300

    tags e botões

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,15 +1,27 @@
 import * as S from './styles'
 import bannerImg from '../../assets/images/banner-homem-aranha.png'
+import Tag from '../Tag'
+import Button from '../Button'
 
 const Banner = () => (
   <S.Imagem style={{ backgroundImage: `url(${bannerImg})` }}>
     <div className="container">
-      <S.Titulo>Marvel&apos;s Spider-Man: Miles Morales PS4 & PS5</S.Titulo>
-      <S.Preços>
-        De <span>R$ 250,00</span>
-        <br />
-        por apenas R$ 99,90
-      </S.Preços>
+      <Tag size="big">Destaque do dia</Tag>
+      <div>
+        <S.Titulo>Marvel&apos;s Spider-Man: Miles Morales PS4 & PS5</S.Titulo>
+        <S.Preços>
+          De <span>R$ 250,00</span>
+          <br />
+          por apenas R$ 99,90
+        </S.Preços>
+      </div>
+      <Button
+        type="link"
+        to="/produto"
+        title="Clique aqui para aproveitar esta oferta"
+      >
+        Aproveitar
+      </Button>
     </div>
   </S.Imagem>
 )
