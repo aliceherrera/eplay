b7779fdebbf6abc6bb7dee4961a7970fd2cf93c6
Alice Herrera <a.aliceherrera@gmail.com>
2024-12-05 12:45:55 -0300

    criando a página de checkout

---
+++ src/pages/Checkout/styles.ts
@@ -0,0 +1,57 @@
+import styled from 'styled-components'
+import { cores } from '../../styles'
+
+type InputGroupProps = {
+  maxWidth?: string
+}
+
+type RowProps = {
+  marginTop?: string
+}
+
+type TabButtonProps = {
+  isActive: boolean
+}
+
+export const Row = styled.div<RowProps>`
+  display: flex;
+  align-items: flex-end;
+  column-gap: 24px;
+  margin-top: ${(props) => props.marginTop || '0'};
+`
+
+export const InputGroup = styled.div<InputGroupProps>`
+  flex: auto;
+
+  max-width: ${(props) => props.maxWidth || '284px'};
+
+  label {
+    font-size: 14px;
+    margin-bottom: 8px;
+    display: block;
+  }
+
+  input {
+    background-color: ${cores.branco};
+    border: 1px solid ${cores.branco};
+    height: 32px;
+    padding: 0 8px;
+    width: 100%;
+  }
+`
+export const TabButton = styled.button<TabButtonProps>`
+  border-radius: 8px;
+  font-size: 14px;
+  font-weight: bold;
+  color: ${cores.branco};
+  background-color: ${(props) => (props.isActive ? cores.verde : cores.preto)};
+  height: 32px;
+  border: none;
+  margin-right: 16px;
+  padding: 0 8px;
+  cursor: pointer;
+
+  img {
+    margin-right: 8px;
+  }
+`
